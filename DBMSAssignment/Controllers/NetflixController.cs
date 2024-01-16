126ac2d8a09bb34f598943e9d0aa46d7c5e9ba02
sandeep <sandy100060@yahoo.in>
2024-01-15 20:07:39 -0600

    Add project files.

---
+++ DBMSAssignment/Controllers/NetflixController.cs
@@ -0,0 +1,73 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DBMSAssignment.Models;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+
+namespace DBMSAssignment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NetflixController : ControllerBase
+    {
+        string connStr = "mongodb://127.0.0.1:27017";
+        string connectionString= "mongodb+srv://sxy59550:mFytjr91igtzLc9K@cluster0.kyfhftb.mongodb.net/?retryWrites=true&w=majority";
+        string databasename = "database";
+        string collectionname = "netflix";
+
+        [HttpGet]
+        public async Task<IEnumerable<Netflix>> Get()
+        {
+            var client = new MongoClient(connectionString);
+            var db = client.GetDatabase(databasename);
+            var collection = db.GetCollection<Netflix>(collectionname);
+            var result = await collection.FindAsync(x => x.ImdbScore > 8);
+            return result.ToList();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddMovie(Netflix netflix)
+        {
+            var client = new MongoClient(connectionString);
+            var db = client.GetDatabase(databasename);
+            var collection = db.GetCollection<Netflix>(collectionname);
+            await collection.InsertOneAsync(netflix);
+            return Ok();
+        }
+
+        [HttpPatch]
+        public async Task<IActionResult> UpdateMovie(Netflix netflix)
+        {
+            var client = new MongoClient(connectionString);
+            var db = client.GetDatabase(databasename);
+            var collection = db.GetCollection<Netflix>(collectionname);
+            var filter = Builders<Netflix>.Filter.Eq("Id", netflix.Id);
+            await collection.ReplaceOneAsync(filter, netflix);
+            return Ok();
+        }
+
+        [HttpDelete]
+        [Route("/{fname}")]
+        public async Task<IActionResult> DeleteMovie(string fname)
+        {
+            var client = new MongoClient(connectionString);
+            var db = client.GetDatabase(databasename);
+            var collection = db.GetCollection<Netflix>(collectionname);
+            await collection.DeleteOneAsync(x => x.Title.ToUpper().Equals(fname.ToUpper()));
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("/{fname}")]
+        public async Task<IEnumerable<Netflix>> GetMovie(string fname)
+        {
+            var client = new MongoClient(connectionString);
+            var db = client.GetDatabase(databasename);
+            var collection = db.GetCollection<Netflix>(collectionname);
+            var result = await collection.FindAsync(x => x.Title.ToUpper().Contains(fname.ToUpper()));
+            return result.ToList();
+        }
+    }
+}
---
+++ DBMSAssignment/Models/netflix.cs
@@ -0,0 +1,88 @@
+﻿
+using MongoDB.Bson.Serialization.Attributes;
+using System.Collections.Generic;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Bson.Serialization;
+using System.Globalization;
+
+namespace DBMSAssignment.Models
+{
+    public class Netflix
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("id")]
+        public string MovieId { get; set; }
+
+        [BsonElement("imdb_score")]
+        public double ImdbScore { get; set; }        
+
+        [BsonElement("age_certification")]
+        public string Age_Certification { get; set; }
+
+        [BsonElement("description")]
+        public string Description { get; set; }
+
+        [BsonElement("genres")]
+        public List<string> Genres { get; set; }
+
+        [BsonElement("production_countries")]
+        public List<string> Production_Countries { get; set; }
+
+        [BsonElement("release_year")]
+        public int Release_Year { get; set; }
+
+        [BsonElement("runtime")]
+        public int Runtime { get; set; }
+
+        [BsonElement("title")]
+        [BsonSerializer(typeof(BsonStringNumericSerializer))]
+        public string Title { get; set; }
+
+        [BsonElement("type")]
+        public string Type { get; set; }
+    }
+
+    public class BsonStringNumericSerializer : SerializerBase<string>
+    {
+        public override string Deserialize(BsonDeserializationContext context, BsonDeserializationArgs args)
+        {
+            var bsonType = context.Reader.CurrentBsonType;
+            switch (bsonType)
+            {
+                case BsonType.Null:
+                    context.Reader.ReadNull();
+                    return null;
+                case BsonType.String:
+                    return context.Reader.ReadString();
+                case BsonType.Int32:
+                    return context.Reader.ReadInt32().ToString(CultureInfo.InvariantCulture);
+                default:
+                    var message = string.Format($"Custom Cannot deserialize BsonString or BsonInt32 from BsonType {bsonType}");
+                    throw new BsonSerializationException(message);
+            }
+        }
+
+        public override void Serialize(BsonSerializationContext context, BsonSerializationArgs args, string value)
+        {
+            if (value != null)
+            {
+                if (int.TryParse(value, out var result))
+                {
+                    context.Writer.WriteInt32(result);
+                }
+                else
+                {
+                    context.Writer.WriteString(value);
+                }
+            }
+            else
+            {
+                context.Writer.WriteNull();
+            }
+        }
+    }
+}
